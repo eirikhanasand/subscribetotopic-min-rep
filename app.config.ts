74570b073f482f961327d7c6c5478631b91d6b5b
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-03 23:13:47 +0200

    Completed minrep

--- app.config.ts
+++ app.config.ts
@@ -3,13 +3,5 @@ import { ExpoConfig, ConfigContext } from '@expo/config'
 export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: 'Login',
-    slug: 'Login',
-    ios: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST,
-        ...config.ios
-    },
-    android: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-        ...config.android
-    }
-});
+    slug: 'Login'
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,41 @@
 import React from 'react';
-import { AppRegistry, Button, Text, View } from 'react-native';
+import { AppRegistry, Button, View } from 'react-native';
 import firebase from '@react-native-firebase/app';
 import messaging from '@react-native-firebase/messaging';
 
 firebase.messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
-});
+    console.log('Message handled in the background!', remoteMessage);
+})
+
 firebase.messaging().onMessage(message => {
-  console.log('on message log');
-});
-n
+    console.log('on message log');
+})
+
 export default function App(props: any) {
     return (
         <View>
-        <Text>text text text</Text>
-        <Text>text text text</Text>
-        <Text>text text text</Text>
-        <Button
-            title="subscribe to topic"
-            onPress={async () => {
-            try {
-                await firebase.messaging().subscribeToTopic('test');
-                console.log('subscribed');
-            } catch (e) {
-                console.log(e);
-            }
-            }}
-        />
-        <Button
-            title="get token"
-            onPress={async () => {
-            try {
-                const token = await messaging().getToken();
-                console.log('token', token);
-            } catch (e) {
-                console.log(e);
-            }
-            }}
-        />
+            <Button
+                title="subscribe to topic"
+                onPress={async () => {
+                    try {
+                        await firebase.messaging().subscribeToTopic('test');
+                        console.log('subscribed');
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }}
+            />
+            <Button
+                title="get token"
+                onPress={async () => {
+                    try {
+                        const token = await messaging().getToken();
+                        console.log('token', token);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }}
+            />
         </View>
     )
 }
